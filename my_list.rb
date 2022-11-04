fa72e5106fc2ac467e5eba2b51de381ce05ee144
Apple <riyabulia12@gmail.com>
2022-11-04 18:54:15 +0530

    Accepting n args instead of 1

--- my_list.rb
+++ my_list.rb
@@ -3,7 +3,7 @@ require_relative 'enumerable'
 class MyList
   include MyEnumerable
 
-  def initialize(arr)
+  def initialize(*arr)
     @list = arr
   end
 
@@ -14,7 +14,7 @@ class MyList
   end
 end
 
-list = MyList.new([1, 2, 3, 4])
+list = MyList.new(1, 2, 3, 4)
 list.all? { |e| e < 5 }
 list.all? { |e| e > 5 }
 list.any? { |e| e == 2 }
